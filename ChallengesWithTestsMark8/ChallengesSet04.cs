75cb6a2ac193a80c419945e9ee4c2c4bd9d9eb78
Daniel <dan.r.french@sbcglobal.net>
2023-06-01 16:15:22 -0500

    completed Weekly Challenges set 04

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +10,114 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            var addEven = numbers.Where(x => x % 2 == 0);
+            var subtractOdd = numbers.Where(x => x % 2 != 0);
+            return addEven.Sum() - subtractOdd.Sum();
+            
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            List<string> strings = new List<string>();
+            strings.Add(str1);
+            strings.Add(str2);
+            strings.Add(str3);
+            strings.Add(str4);
+            var shortestString = strings.Min(s => s.Length);
+            return shortestString;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            List<int> numbers = new List<int>();
+            numbers.Add(number1);
+            numbers.Add(number2);
+            numbers.Add(number3);
+            numbers.Add(number4);
+            var smallestNumber = numbers.Min();
+            return smallestNumber;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            var newName = new Business();
+            newName.Name = "TrueCoders";
+            biz.Name = newName.Name;
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            
+           if (sideLength1 + sideLength2 > sideLength3 && sideLength2 + sideLength3 > sideLength1 && sideLength1 + sideLength3 > sideLength2)
+            {
+                return true;  
+            }
+           else
+            {
+                return false;
+            }
+             
+
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            bool isNumber = double.TryParse(input, out double number);
+            if (isNumber)
+            {
+                return true;
+            }
+            else { return false; }
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int total = objs.Length;
+            int nulls = objs.Count(element  => element == null);
+            bool isMajorityNull = nulls > total / 2; 
+            return isMajorityNull;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+            
+            int evenSum = 0;
+            int evensCount = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                    evensCount++;
+                }
+            }
+            double evenAverage = evensCount > 0 ?  Convert.ToDouble(evenSum) / evensCount : 0;
+            return evenAverage;     
+                                         
+    
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
-        }
+
+            if (number  < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number), "Factorial is not defined for negative numbers");
+            }
+            
+            if (number == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number-1);
+
+            }
+        }  
     }
 }
